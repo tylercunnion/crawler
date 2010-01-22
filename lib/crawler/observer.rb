911007723682e85980975bdb9286fa1fa448640b
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-01-22 12:50:22 -0500

    Observer tests - prints 404 info

--- lib/crawler/observer.rb
+++ lib/crawler/observer.rb
@@ -1,7 +1,14 @@
 module Crawler
   class Observer
     
-    def update
+    def initialize(log=$stdout)
+      @log = log
+    end
+    
+    def update(errcode, url)
+      if errcode == "404"
+        @log.puts "404 encountered for " + url
+      end
     end
   end
 end
--- spec/crawler/observer_spec.rb
+++ spec/crawler/observer_spec.rb
@@ -1,7 +1,23 @@
 require File.join(File.dirname(__FILE__), "..", "spec_helper.rb")
+require 'stringio'
 
 module Crawler
   describe Observer do
+    
+    it "should output a warning when 404 is reached" do
+      log = double('log')
+      obs = Observer.new(log)
+      log.should_receive(:puts).with("404 encountered for http://example.com/")
+      obs.update("404", "http://example.com/")      
+    end
+    
+    it "should not output a warning when 200 is encountered" do
+      log = double('log')
+      obs = Observer.new(log)
+      log.should_not_receive(:puts)
+      obs.update("200", "http://example.com/")
+    end
+    
 
   end
 end
