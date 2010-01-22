1bf92c0e5ef1e208ef4ccc361b4368ab9cefa512
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-01-21 22:55:48 -0500

    Set up testing through WEBrick

--- spec/crawler/crawler_spec.rb
+++ spec/crawler/crawler_spec.rb
@@ -9,7 +9,50 @@ module Crawler
         crawler.crawled.should be_empty
       end
     end
-    
+      
+    context "during a crawl" do
+        
+        before(:all) do
+          @uri_base = 'http://localhost:12000/'
+          www_root = File.dirname(__FILE__) + '/../fixtures/'
+          @server = Thread.new do
+            s = WEBrick::HTTPServer.new({:Port => 12000, :DocumentRoot => www_root, :AccessLog => []})
+            @port = s.config[:Port]
+            begin
+              s.start
+            ensure
+              s.shutdown
+            end
+          end
+        end
+        
+        before(:each) do
+          @crawler = Webcrawler.new
+          @obs = mock("observer", :update => nil)
+          @crawler.add_observer(@obs)
+        end
+
+        it "should send notifications" do
+          uri = URI.parse(@uri_base)
+          
+          @obs.should_receive(:update)
+          @crawler.crawl(uri)
+        end
+        
+        it "should send status code and URL" do
+          uri = URI.parse(@uri_base)
+          @obs.should_receive(:update).with("200", uri.to_s)
+          @crawler.crawl(uri)
+        end
+        
+        it "should send 404 for missing URL" do
+          uri = URI.parse(@uri_base + 'doesnotexist.html')
+          @obs.should_receive(:update).with("404", uri.to_s)
+          @crawler.crawl(uri)
+        end
+
+      end
+      
     context "after crawl" do
       before(:each) do
         @crawler = Webcrawler.new
@@ -20,38 +63,6 @@ module Crawler
       it "should have at least one item in crawled" do
         @crawler.crawled.should_not be_empty
       end
-      
-      context "during a crawl" do
-        
-        before(:each) do
-          @crawler = Webcrawler.new
-          @obs = mock("observer", :update => nil)
-          @crawler.add_observer(@obs)
-          @uri = URI.parse('http://example.com/')
-        end
-
-        it "should send notifications" do
-          uri = URI.parse('http://example.com/')
-          
-          @obs.should_receive(:update)
-          @crawler.crawl(uri)
-        end
-        
-        
-        it "should send status code and URL" do
-          uri = URI.parse('http://example.com/')
-          @obs.should_receive(:update).with("200", uri.to_s)
-          @crawler.crawl(uri)
-        end
-        
-        it "should send 404 for missing URL" do
-          uri = URI.parse('http://example.com/fartz/')
-          @obs.should_receive(:update).with("404", uri.to_s)
-          @crawler.crawl(uri)
-        end
-
-      end
-        
     end
   end
 end
