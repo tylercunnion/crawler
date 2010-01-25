06d6e81dbae1d5b570cda6687bf1ce41b56aaa2a
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-01-25 00:20:00 -0500

    Added options, timeout

--- lib/crawler/webcrawler.rb
+++ lib/crawler/webcrawler.rb
@@ -2,6 +2,7 @@ require 'set'
 require 'observer'
 require 'net/http'
 require 'nokogiri'
+require 'timeout'
 
 module Crawler
   class Webcrawler
@@ -10,31 +11,38 @@ module Crawler
     
     attr_accessor :crawled
     attr_accessor :queue
+    attr_accessor :options
     
-    def initialize()
+    def initialize(options={})
       @crawled = Set.new
       @queue = []
+      @options = {
+        :timeout => 1.0/0 #Infinity
+      }.merge(options)
+      
     end
     
     def crawl(start_uri)
       start_uri = start_uri.normalize
       @queue << start_uri
       
-      while(uri = @queue.shift)
-        resp = Net::HTTP.get_response(uri)
+      timeout(@options[:timeout]) {
+        while(uri = @queue.shift)
+          resp = Net::HTTP.get_response(uri)
 
-        changed
-        notify_observers(resp, uri)
+          changed
+          notify_observers(resp, uri)
       
-        html = Nokogiri.parse(resp.body)
-        a_tags = html.search("a")
-        @queue = @queue + a_tags.collect do |t| 
-          next_uri = uri + t.attribute("href").to_s
-          next_uri unless @crawled.include?(next_uri)
+          html = Nokogiri.parse(resp.body)
+          a_tags = html.search("a")
+          @queue = @queue + a_tags.collect do |t| 
+            next_uri = uri + t.attribute("href").to_s
+            next_uri unless @crawled.include?(next_uri)
+          end
+          @queue = @queue.uniq
+          @crawled << uri
         end
-        @queue = @queue.uniq
-        @crawled << uri
-      end
+      }
       
     end
   end
